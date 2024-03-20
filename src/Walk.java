4c86b0c4e8f0abf50580168144a86445daa5422d
yeonsang <wjddustkd45@gmail.com>
2024-03-20 18:51:52 +0900

    2024-03-20 commit

--- src/Walk.java
+++ src/Walk.java
@@ -5,7 +5,11 @@ public class Walk {
     public static int[] solution(String[] park, String[] routes) {
 
 
-
+        /*
+        park의 length 는 h 값
+        park[0] 의 length 는 w 값
+        시작 좌표를 찾아서 routes for 문으로 돌리면 된다.
+         */
 
 
         int[] answer = {};
