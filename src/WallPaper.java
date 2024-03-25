6a75473aebe2e6d88b5c94a9605924e54e6baceb
yeonsang <wjddustkd45@gmail.com>
2024-03-25 22:24:34 +0900

    2024-03-25 programmers 배경화면 정리 풀이

--- src/WallPaper.java
+++ src/WallPaper.java
@@ -6,37 +6,47 @@ public class WallPaper {
     public static int[] solution(String[] wallpaper) {
 
         char[][] arr = new char[wallpaper.length][wallpaper[0].length()];
-        List<Integer> list = new ArrayList<>(); // h list
-        List<Integer> list2 = new ArrayList<>(); // w list
+        List<Integer[]> list = new ArrayList<>(); // h list
+
+        for(int i = 0; i < wallpaper.length; i ++){
+            arr[i] = wallpaper[i].toCharArray();
+        }
         for(int i = 0; i < wallpaper.length; i++){
             for(int j = 0; j < wallpaper[0].length(); j ++ ){
                 if(arr[i][j] == '#') {
-                    list.add(i);
-                    list2.add(j);
+                    Integer[] arr2 = new Integer[2];
+                    arr2[0] = i;
+                    arr2[1] = j;
+                    list.add(arr2);
                 }
             }
         }
+
         int maxW = 0;
         int maxH = 0;
-        int minW = wallpaper[0].length();
-        int minH = wallpaper.length;
+        int minW = wallpaper[0].length() -1;
+        int minH = wallpaper.length -1;
         for(int i = 0; i < list.size(); i++){
-            if(maxH < list.get(i)) {
-                maxH = list.get(i);
+            Integer[] arr3 = list.get(i);
+            if(maxH < arr3[0]) {
+                maxH = arr3[0];
+            }
+
+            if(maxW < arr3[1]) {
+                maxW = arr3[1];
             }
         }
 
-        for(int i = 0; i < list2.size(); i ++){
-            if(maxW < list2.get(i)) {
-                maxW = list2.get(i);
+        for(int i =0; i < list.size(); i++){
+            Integer[] arr3 = list.get(i);
+            if(minH > arr3[0]) {
+                minH = arr3[0];
+            }
+
+            if(minW > arr3[1]) {
+                minW = arr3[1];
             }
         }
-
-
-        System.out.println(maxH);
-        System.out.println(maxW);
-        System.out.println(minH);
-        System.out.println(minW);
         int[] arr4 = new int[4];
         arr4[0] = minH;
         arr4[1] = minW;
@@ -44,7 +54,7 @@ public class WallPaper {
         arr4[3] = maxW + 1;
 
         int[] answer = arr4;
-        System.out.println(Arrays.toString(answer));
+
         return answer;
     }
     public static void main(String[] args) {
