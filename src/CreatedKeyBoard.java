c871a020ec99408c772da182d0e001b8f158db34
yeonsang <wjddustkd45@gmail.com>
2024-03-27 22:01:59 +0900

    2024-03-27 대충만든 키패드 완

--- src/CreatedKeyBoard.java
+++ src/CreatedKeyBoard.java
@@ -1,23 +1,49 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class CreatedKeyBoard {
 
     public static int[] solution(String[] keymap, String[] targets) {
 
-        // targets 에 있는 문자열을 차례대로 돌려서 keymap 에 있는 애들 중에 인덱스가 가장 작은 애들을 고르기
-        // keymap 의 각 원소들을 선택할때마다 count ++
-        char[][] arr = new char[targets.length][targets[0].length()];
+        /*
+        keymap 의 원소 자기 자신에서 중복된 원소들을 넣으면됨. 하지만 중복되는 char 의 경우에는 가장 먼저 입력한 index만 넣으면 됨.
+        하지만 {A=1, B=2, C=4, D=5, E=3, F=4} 이렇게 나옴.. B 는 2번 키 1번만 누르면 되는건데...
+        */
+        Map<Character, Integer> map = new HashMap<>(); // map 에 각 keymap 의 알파벳들의 최솟값이 찍힘.
+        for(int i = 0; i < keymap.length; i++){
+            for(int j = 0; j < keymap[i].length(); j++){
+                char arr = keymap[i].charAt(j);
+                if(map.containsKey(arr)) {
+                    int idx = map.get(arr);
+                    map.put(arr, Math.min(idx, j+1));
+                }else{
+                    map.put(arr, j+1);
+                }
 
-
-
-        int[] answer = {};
+            }
+        }
+        int[] result = new int[targets.length];
+        for(int i = 0; i < targets.length; i++){
+            String target = targets[i];
+            int count = 0;
+            boolean flag = true;
+            for(char c : target.toCharArray()){
+                if(map.containsKey(c)){
+                    count += map.get(c);
+                }else{
+                    flag = false;
+                    break;
+                }
+            }
+            result[i] = flag ? count : -1;
+        }
+        System.out.println(Arrays.toString(result));
+        int[] answer = result;
         return answer;
     }
     public static void main(String[] args) {
         String[] keymap = {"ABACD", "BCEFD"};
         String[] targets = {"ABCD","AABB"};
-
+        // result => [9,7]
         solution(keymap, targets);
     }
 }
