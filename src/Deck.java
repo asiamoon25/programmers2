9611a81f08d0b8f8cc03fd0871264c6efdf7f3c1
yeonsang <wjddustkd45@gmail.com>
2024-03-29 15:39:48 +0900

    2024-03-29 카드덱 문제 완

--- src/Deck.java
+++ src/Deck.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Deck {
     public static String solution(String[] cards1, String[] cards2, String[] goal) {
         /*
@@ -5,10 +9,18 @@ public class Deck {
         각 배열의 원소들은 1번만 사용할 수 있음.
         그러면 goal 을 돌리면서 card1, card2 덱을 한번 씩 사용해서
          */
+        String answer ="Yes";
+        List<String> list1 = new ArrayList<>(Arrays.asList(cards1));
+        List<String> list2 = new ArrayList<>(Arrays.asList(cards2));
         for(int i = 0; i < goal.length; i++){
-
+            if(list1.size() != 0 && goal[i].equals(list1.get(0))) {
+                list1.remove(0);
+            }else if(list2.size() != 0 && goal[i].equals(list2.get(0))) {
+                list2.remove(0);
+            }else{
+                answer = "No";
+            }
         }
-        String answer = "";
         return answer;
     }
 
