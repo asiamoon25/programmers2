7b8536eca6e27d1ad4f9698a8f78393c33a94578
yeonsang <wjddustkd45@gmail.com>
2024-04-12 18:50:55 +0900

    2024-04-12 프로세스 풀이 중

---
+++ src/stackandqueue/Process.java
@@ -0,0 +1,23 @@
+package stackandqueue;
+
+import java.util.*;
+
+public class Process {
+    public static int solution(int[] priorities, int location) {
+
+        for(int i = 0; i < priorities.length; i++){
+            int priority = priorities[i];
+            int l = i;
+
+
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] priorities = {2,1,3,2};
+        int location = 2;
+        solution(priorities,location);
+    }
+}
