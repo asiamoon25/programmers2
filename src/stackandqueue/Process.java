7440e0a00cfaa15f661178f31d0452ff32375b21
yeonsang <wjddustkd45@gmail.com>
2024-04-29 13:44:59 +0900

    2024-04-29 프로그래머스 process 풀이 완

--- src/stackandqueue/Process.java
+++ src/stackandqueue/Process.java
@@ -6,33 +6,29 @@ import java.util.*;
 
 public class Process {
     public static int solution(int[] priorities, int location) {
-        /*
-        1. 실행 대기 큐(Queue) 에서 대기중인 프로세스를 하나 꺼냄.
-        2. 큐에 대기중인 프로세스 중 우선순위가 더 높은 프로세스가 있다면 방금 꺼낸 프로세스를 다시 큐에 넣음
-        3. 만약 그런 프로세스가 없다면 방금 꺼낸 프로세스를 실행함.
-            3.1 한 번 실행한 프로세스는 다시 큐에 넣지 않고 그대로 종료됨.
 
-        Queue 준비
-        우선순위의 index, priority 세트인 int[] 을 저장
-        location 을 받아섯 해당 index 의 값을 찾아야됨.
-        최대값을 뽑은 후 Queue 를 최대값의 index 아래로 순서대로 조정하면 됨.
+        /*
+        PriorityQueue
          */
-        Deque<int[]> q2 = new LinkedList<>();
-        for(int i = 0; i < priorities.length; i++){
-            q2.add(new int[]{i,priorities[i]});
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        int answer = 0;
+
+        for (int i : priorities) {
+            queue.add(i);
         }
-        /*
-        2 1 3 2
-        2 > 1 ? 2 3 2 1
-        2 > 3 ? 3 2 1 2
-        3 > 2 ? 3 1 2 2
-        3 > 1 ? 3 2 2 1
-         */
-        int[] max = q2.pollFirst();
 
-
-
-        return 0;
+        while (!queue.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+                if (queue.peek() == priorities[i]) {
+                    queue.poll();
+                    answer++;
+                    if (location == i) {
+                        return answer;
+                    }
+                }
+            }
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
